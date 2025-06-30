6ba575d7807fcf61354af9bc792038bb48907f9c
Amit4218 <amitbhagat621@gmail.com>
2025-06-30 21:58:35 +0530

    changes of plan in ui

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from db import get_random_riddle
 
 app = Flask(__name__)
@@ -7,7 +7,12 @@ app = Flask(__name__)
 @app.route("/get-random-riddle")
 def random_riddle():
     riddle = get_random_riddle()
-    return jsonify(riddle)
+    return render_template("riddle.html", questions=riddle)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")    
 
 
 if __name__ == "__main__":
